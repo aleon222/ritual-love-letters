34b9981df1c1cce370ea1338ca17f1a4e26932e4
alyssa colette <136546965+aleon222@users.noreply.github.com>
2024-04-09 16:24:49 -0400

    Style updates + BringToFront()

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     //New letter button
     newLetterGenerator()
+
   })
 
 
@@ -50,7 +51,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // make each new Letter draggable
     dragElement(loveLetterDivElement);
-    
     divCounter++
     })
   }
@@ -62,6 +62,7 @@ document.addEventListener("DOMContentLoaded", () => {
     displayLetter(quotesArray[randomQuote], newLetter);
 
     //delete entry from array once displayed
+    quotesArray.splice(randomQuote, 1);
   }
 
   //DISPLAY LETTERS
@@ -81,51 +82,50 @@ document.addEventListener("DOMContentLoaded", () => {
 const quotesArray = [
   `“What if it is not an experiment, this creative life of refusal. This rarely institutionally-funded disloyal life of practice for a world as yet unnamable. This insistence on transformation when the bank account screams “conform.” What if this life as an independent experimental artist is not itself an independent experiment? What if it’s an interdependent ceremony?” 
   
-  -- Alexis Pauline Gumbs :: "<a href="https://topicalcream.org/features/the-god-of-every-day/">The God of Every Day</a>"`,
+  <br> -- Alexis Pauline Gumbs :: "<a href="https://topicalcream.org/features/the-god-of-every-day/" target="_blank">The God of Every Day</a>"`,
 	
   `“It doesn't matter if you think the goals are unattainable. They are. What matters is that they are impossible without the work, they cannot happen if you don't make the work.”
 	
-  -- Awkwaeke Emezi :: "<i>Dear Senthuran: Black Spirit Memoir</i>", Execution | Dear Nonso`,
+  <br> -- Awkwaeke Emezi :: "<i>Dear Senthuran: Black Spirit Memoir</i>", Execution | Dear Nonso`,
                 
   `“Sometimes we don't get the reassurances we want; we make the work anyway.”
   
-  -- Awkwaeke Emezi :: "<i>Dear Senthuran: Black Spirit Memoir</i>", Canon | Dear Daniel`,
+  <br> -- Awkwaeke Emezi :: "<i>Dear Senthuran: Black Spirit Memoir</i>", Canon | Dear Daniel`,
 
   `“First forget inspiration. Habit is more dependable. Habit will sustain you whether you’re inspired or not. Habit will help you finish and polish your stories. Inspiration won’t. Habit is persistence in practice.”
   
-  -- Octavia Butler`,
+  <br> -- Octavia Butler :: <i>Bloodchild and Other Stories</i>, Furor Scribendi`,
 
   `“The white fathers have told us: 'l think, therefore I am.' But the Black mother within each one of us—the poet inside—whispers in our dreams: 'I feel, therefore I can be free.' Learn to use what you feel to move you toward action. Change, personal and political, does not come about in a day, nor a year. But it is our day-to-day decisions, the way in which we testify with our lives to those things in which we say we believe, that empower us. Your power is relative, but it is real. And if you do not learn to use it, it will be used, against you, and me, and our children. Change did not begin with you, and it will not end with you, but what you do with your life is an absolutely vital piece of that chain. The testimony of your daily living is the missing remnant in the fabric of our future.”
   
-  -- Audre Lorde :: Oberlin College May 29, 1989`,
+  <br> -- Audre Lorde :: Oberlin College May 29, 1989`,
 
   `“They told me it was a hallucination, this waking dream I want for all of us, but now I know the truth. In a world where capitalism as usual makes complicit in drowning the planet, we are the ones who are already dolphins, the psychics, the visionaries. We could trust ourselves. Our adaptable foreheads were not made to be caged; we deserve the restful freedom to evolve, to -- as D'atra Jackson said at the North Caroline Emergent Strategy Immersion -- ‘surrender to your dreams’”
   
-  -- Alexis Pauline Gumbs :: "<i>Undrowned</i>", Honor your Boundaries`,
+  <br> -- Alexis Pauline Gumbs :: "<i>Undrowned</i>", Honor your Boundaries`,
 
   `“Our fear may not go away, but it will not stand in the way”
   
-  -- bell hooks :: pulled from Seed a World Affirmations`,
+  <br> -- bell hooks :: pulled from <a href="https://www.seedaschool.com/program" target="_blank">Seed a World</a> Affirmations`,
 
   `“The moment we choose to love, we begin to move against domination, against oppression. The moment we choose to love we begin to move towards freedom, to act in ways that liberate ourselves and others. That action is the testimony of love as the practice of freedom”
   
-  -- bell hooks :: Love as the Practice of Freedom`,
+  <br> -- bell hooks :: Love as the Practice of Freedom`,
 
   `“How would we spend our time if we realized that the conflicts we are experiencing now urgently demand that we create more loving world as soon as possible?”
   
-  -- Alexis Pauline Gumbs :: "<i>Undrowned</i>", Learn from Conflict`,
+  <br> -- Alexis Pauline Gumbs :: "<i>Undrowned</i>", Learn from Conflict`,
 
   `“Whether it’s been years, months or weeks since you’ve released work or visited the altar of your creative practice, it’s always the right time to return. Returning to our way of knowing, being and breathing reminds us of the possibility always available to us inside the non-linear loop of creative practice. If fear is excitement without breath, love lets the oxygen in. This radical self-love makes burn out much more difficult to take hold and easier to spot before it begins. Returning to desire is a practice of recharge, not depletion. Inside our breathwork, the spacious possibility that creative practice cultivates, we get to create worlds that nurture our wholeness and attract resources, relationships and opportunities we couldn’t imagine would find their way to us. But first, we must find our way back to ourselves…
   Over and
   Over and
   Over again.”
 
-  -- Ayana Zaire Cotton :: "<a href="https://open.substack.com/pub/seedaschool/p/practice-doesnt-make-perfect-it-makes?r=aayhw&utm_campaign=post&utm_medium=web">Practice Doesn’t Make Perfect, It Makes Possibility</a>"` 
+  <br> -- Ayana Zaire Cotton :: "<a href="https://open.substack.com/pub/seedaschool/p/practice-doesnt-make-perfect-it-makes?r=aayhw&utm_campaign=post&utm_medium=web" target="_blank">Practice Doesn’t Make Perfect, It Makes Possibility</a>"` 
 ]
 
 
 //CYCLE THROUGH COLOR PALETTER
-  // Define color values
   const colors = ['#edcb59', '#BFA1B5', '#C4F16A'];
   let colorIndex = 0;
 
@@ -141,7 +141,6 @@ const quotesArray = [
       element.style.borderColor = color;
       element.querySelector('.top').style.backgroundColor = color;
   }
-
 
 //DRAG LETTER FUNCTION
 function dragElement(elmnt) {
@@ -161,6 +160,8 @@ function dragElement(elmnt) {
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
+    // Bring the element to the front when drag starts
+    bringToFront(elmnt);
   }
 
   function elementDrag(e) {
@@ -181,4 +182,21 @@ function dragElement(elmnt) {
     document.onmouseup = null;
     document.onmousemove = null;
   }
+
 }
+
+//BRING TO FRONT
+// Initialize counter
+  var zIndexCounter = 1;
+
+  function bringToFront(element) {
+  zIndexCounter++;
+  // Set the z-index of the clicked element to the new counter value
+  element.style.zIndex = zIndexCounter;
+  console.log("z counter", zIndexCounter)
+  }
+
+  // Attach click event listeners to all .love-letter elements
+  document.querySelector(".love-letter").addEventListener('click', function() {
+    bringToTop(this);
+  });
